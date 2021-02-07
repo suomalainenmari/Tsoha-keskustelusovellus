4dbd2741083c9f32fe51cbd8463726e7bb09cb06
Mari Suomalainen <mariannikasuomalainen@gmail.com>
2021-02-07 19:06:15 +0200

    Correcting an error in SQL  schema

--- schema.sql
+++ schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     user_id INTEGER REFERENCES users,
-    sent_at TIMESTAMP
+    sent_at TIMESTAMP,
     subject_id INTEGER REFERENCES subjects
 
 );
